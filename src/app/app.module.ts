9afb13dfb0893121ed50df7984dd2fbcdee09203
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2022-02-25 16:58:46 -0600

    Empty components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PlaylistComponent } from './playlist/playlist.component';
+import { PlaylistTrackComponent } from './playlist/playlist-track/playlist-track.component';
+import { UserInputComponent } from './user-input/user-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaylistComponent,
+    PlaylistTrackComponent,
+    UserInputComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/playlist/playlist-track/playlist-track.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-playlist-track',
+  templateUrl: './playlist-track.component.html',
+  styleUrls: ['./playlist-track.component.css']
+})
+export class PlaylistTrackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
