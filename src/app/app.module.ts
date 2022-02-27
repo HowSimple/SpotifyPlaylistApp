2a61299c06d4616bc660b2f8b2427fd337d1348d
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2022-02-27 17:20:50 -0600

    Added form to body component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PlaylistComponent } from './playlist/playlist.component';
-import { PlaylistTrackComponent } from './playlist/playlist-track/playlist-track.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import { DetailsComponent } from './details/details.component';
 import {FormsModule} from "@angular/forms";
@@ -13,7 +12,6 @@ import { BodyComponent } from './body/body.component';
   declarations: [
     AppComponent,
     PlaylistComponent,
-    PlaylistTrackComponent,
     UserInputComponent,
     DetailsComponent,
     BodyComponent
